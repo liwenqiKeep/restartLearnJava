eaba3810394ed2b9be3b400af1abb601caee6720
liwenqiKeep <278956314@qq.com>
2022-06-27 11:17:46 +0800

    LeetCode14,20,23

--- 05_面试&算法题集/LeetCode/src/main/java/com/lwq/leetcode/easy/No14.java
+++ 05_面试&算法题集/LeetCode/src/main/java/com/lwq/leetcode/easy/No14.java
@@ -5,32 +5,40 @@ package com.lwq.leetcode.easy;
  */
 public class No14 {
     public static void main(String[] args) {
+        String[] arr1 = {"flower", "flow", "flight"};
+        System.out.println(longestCommonPrefix(arr1));
+
+        String[] arr2 = {"dog", "racecar", "car"};
+
+        System.out.println(longestCommonPrefix(arr2));
+
+        String[] arr3 = {"cir", "car"};
+
+        System.out.println(longestCommonPrefix(arr3));
+
+        String[] arr4 = {"reflower", "flow", "flight"};
+
+        System.out.println(longestCommonPrefix(arr4));
 
     }
 
-    public String longestCommonPrefix(String[] strs) {
-        String preF = "";
-        if (strs.length < 1) {
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) {
             return "";
         }
-        if (strs.length == 1) {
-            return strs[0];
-        }
-        boolean flag = true;
-        int index = 0;
-        int a = 0;
-        while (flag) {
-
-            if (strs[a].substring(0, index).equals(strs[a + 1].substring(0, index))) {
-                preF = strs[a].substring(0, index);
-                a ++;
-            } else {
-                flag = false;
+        String ans = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            int j = 0;
+            for (; j < ans.length() && j < strs[i].length(); j++) {
+                if (ans.charAt(j) != strs[i].charAt(j)) {
+                    break;
+                }
             }
-            if(a > strs.length-2){
-                continue;
+            ans = ans.substring(0, j);
+            if (ans.equals("")) {
+                return ans;
             }
         }
-        return preF;
+        return ans;
     }
 }
---
+++ 05_面试&算法题集/LeetCode/src/main/java/com/lwq/leetcode/easy/No23.java
@@ -0,0 +1,30 @@
+package com.lwq.leetcode.easy;
+
+/**
+ * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+ * @author liwenqi
+ */
+public class No23 {
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+        return null;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
